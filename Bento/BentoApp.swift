9e49370e94280088e589793a4c8b9701ea6f77fc
patelarth0311 <patelarth0311@gmail.com>
2023-03-26 12:48:57 -0400

    Initial Commit

---
+++ Bento/BentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BentoApp.swift
+//  Bento
+//
+//  Created by Arth Patel on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
